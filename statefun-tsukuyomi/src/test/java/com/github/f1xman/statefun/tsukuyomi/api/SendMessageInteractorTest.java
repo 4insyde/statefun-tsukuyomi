38635ec6ebec2796f4ec94f84f17278f28ee6a7b
Tymur Yarosh <tyarosh@intellias.com>
2022-04-12 11:11:17 +0300

    cover with tests

---
+++ statefun-tsukuyomi/src/test/java/com/github/f1xman/statefun/tsukuyomi/api/SendMessageInteractorTest.java
@@ -0,0 +1,73 @@
+package com.github.f1xman.statefun.tsukuyomi.api;
+
+import com.github.f1xman.statefun.tsukuyomi.core.TsukuyomiApi;
+import com.github.f1xman.statefun.tsukuyomi.core.capture.Envelope;
+import org.apache.flink.statefun.sdk.java.TypeName;
+import org.apache.flink.statefun.sdk.java.types.Type;
+import org.apache.flink.statefun.sdk.java.types.TypeSerializer;
+import org.apache.flink.statefun.sdk.java.types.Types;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Base64;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class SendMessageInteractorTest {
+
+    static final String FOO_BAR = "foo/bar";
+
+    @Mock
+    TsukuyomiApi mockedTsukuyomiApi;
+
+    @Test
+    void extractsCollaboratorFromEnvelope() {
+        Envelope envelope = envelope();
+        SendMessageInteractor interactor = SendMessageInteractor.of(envelope);
+        TypeName expected = TypeName.typeNameFromString(FOO_BAR);
+
+        Optional<TypeName> actual = interactor.getCollaborator();
+
+        assertThat(actual).contains(expected);
+    }
+
+    @Test
+    void returnsEmptyCollaboratorIfFromIsNull() {
+        Envelope ingressEnvelope = envelope().toBuilder()
+                .from(null)
+                .build();
+        SendMessageInteractor interactor = SendMessageInteractor.of(ingressEnvelope);
+
+        Optional<TypeName> actual = interactor.getCollaborator();
+
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void sendsEnvelope() {
+        Envelope envelope = envelope();
+        SendMessageInteractor interactor = SendMessageInteractor.of(envelope);
+
+        interactor.interact(mockedTsukuyomiApi);
+
+        then(mockedTsukuyomiApi).should().send(envelope);
+    }
+
+    private Envelope envelope() {
+        Type<String> stringType = Types.stringType();
+        TypeSerializer<String> serializer = stringType.typeSerializer();
+        return Envelope.builder()
+                .from(Envelope.NodeAddress.of(FOO_BAR, "foobar"))
+                .to(Envelope.NodeAddress.of("foo/baz", "foobaz"))
+                .data(Envelope.Data.of(
+                        stringType.typeName().asTypeNameString(),
+                        Base64.getEncoder().encodeToString(serializer.serialize("foobarbaz").toByteArray())
+                ))
+                .build();
+    }
+}
