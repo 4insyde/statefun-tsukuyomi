9a1fe3f7b1e528f2eaed104f0cbf9f6c478f8b93
Tymur Yarosh <ti.yarosh@gmail.com>
2022-05-06 16:05:43 +0300

    implement validate for criteria and deprecate the one accepting change matchers

--- statefun-tsukuyomi/src/main/java/com/github/f1xman/statefun/tsukuyomi/core/validation/ValidationRunner.java
+++ statefun-tsukuyomi/src/main/java/com/github/f1xman/statefun/tsukuyomi/core/validation/ValidationRunner.java
@@ -1,5 +1,8 @@
 package com.github.f1xman.statefun.tsukuyomi.core.validation;
 
 public interface ValidationRunner {
+    @Deprecated
     void validate(ChangeMatcher... changeMatchers);
+
+    void validate(Criterion... criteria);
 }
