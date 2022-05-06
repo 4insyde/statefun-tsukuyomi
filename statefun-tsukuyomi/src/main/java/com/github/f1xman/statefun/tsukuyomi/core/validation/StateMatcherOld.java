7d825b70e6c2a8d673e4a86459d7abdb22923427
Tymur Yarosh <ti.yarosh@gmail.com>
2022-05-06 17:39:07 +0300

    remove components of machers-based implementation

--- statefun-tsukuyomi/src/main/java/com/github/f1xman/statefun/tsukuyomi/core/validation/StateMatcherOld.java
+++
@@ -1,9 +0,0 @@
-package com.github.f1xman.statefun.tsukuyomi.core.validation;
-
-import com.github.f1xman.statefun.tsukuyomi.core.dispatcher.TsukuyomiApi;
-
-public interface StateMatcherOld {
-
-    void match(TsukuyomiApi tsukuyomi);
-
-}
